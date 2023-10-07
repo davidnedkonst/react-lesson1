a3b3dcbdb6ab350eb87e2ae81bfb9cd5255188cf
davidnedkonst <david.ned.kon@gmail.com>
2023-10-07 14:20:15 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const span1 = <span>Hello</span>
+const span2 = <span> World</span>
+
+const element1 =
+  <div>
+    {span1}
+    {span2}
+  </div>;
+
+console.log(element1);
+ReactDOM.render(element1, document.querySelector('#root'))
